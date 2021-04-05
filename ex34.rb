436e9ae4d8b43c12503cceb8fe9c1f9030719aa0
hansori01 <19sorihan88@gmail.com>
2021-04-05 23:12:24 +0000

    adding final exercise

---
+++ ex34.rb
@@ -0,0 +1,21 @@
+# animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# puts animals[0]
+
+
+#branches and functions
+
+def benchmark
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  running_time = end_time - start_time
+end
+ 
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple"*100
+ 
+running_time = benchmark { long_string.reverse }
+ 
+puts "string.reverse took #{running_time} seconds to run"
